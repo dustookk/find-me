7b8bb2766c9a88201f7a14ec87cb065db584096e
dustookk <dustookk@gmail.ciom>
2013-05-10 23:26:09 +0800

    dpt赶紧帮我改

---
+++ src/com/gdg/findme/GuideActivity.java
@@ -0,0 +1,20 @@
+package com.gdg.findme;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Setting Guide
+ * 
+ * @author gaoyihang
+ * 
+ */
+public class GuideActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_guide);
+	}
+
+}
