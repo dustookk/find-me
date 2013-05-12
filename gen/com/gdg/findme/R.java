6505a09b667876843bbe38e12416a724679bf406
momogzp <ihsan.gaozp@gmail.com>
2013-05-12 23:40:40 +0800

    加入了定位的位置信息，但是并没有对返回的位置信息进行处理，并且没有进行真机测试。

--- gen/com/gdg/findme/R.java
+++ gen/com/gdg/findme/R.java
@@ -10,6 +10,11 @@ package com.gdg.findme;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int orange=0x7f040000;
+        public static final int system_selector=0x7f040002;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +22,49 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int general__shared__switch_normal=0x7f020000;
+        public static final int general__shared__switch_selected=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int kn_protect_guide_first=0x7f020003;
+        public static final int kn_protect_guide_second=0x7f020004;
+        public static final int kn_protect_guide_third=0x7f020005;
+        public static final int sort_bg=0x7f020006;
+        public static final int xuxian=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090009;
+        public static final int iv_system_observe=0x7f090005;
+        public static final int iv_timeDirection_logo=0x7f090007;
+        public static final int ll_sys_about=0x7f090008;
+        public static final int parent=0x7f090001;
+        public static final int scroll_view=0x7f090002;
+        public static final int slidelayout=0x7f090000;
+        public static final int tv_sys_lasttime=0x7f090003;
+        public static final int tv_system_about=0x7f090006;
+        public static final int tv_system_observe=0x7f090004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_guide=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_guide1=0x7f030002;
+        public static final int fragment_guide2=0x7f030003;
+        public static final int fragment_guide3=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int bt_fragment_guide_next=0x7f060004;
+        public static final int hello_world=0x7f060002;
+        public static final int kn_protect_guide_first=0x7f060003;
+        public static final int kn_protect_guide_second=0x7f060005;
+        public static final int kn_protect_guide_third=0x7f060006;
     }
     public static final class style {
         /** 
@@ -59,10 +88,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
